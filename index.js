997698afaa16263ac1d84e708804bd730d4a5ff7
Vlad Vitan <vlad.vitan@stud.acs.upb.ro>
2020-09-09 15:43:40 +0300

    Changing port.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(http);
 const nedb = require('nedb');
 const dayjs = require('dayjs');                                               
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
